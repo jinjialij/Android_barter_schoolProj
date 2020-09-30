de1d1909d7a23bb63558750cfcadbfc15ffdc86d
Yucheng <yc270707@dal.ca>
2020-09-30 22:09:35 +0800

    registration page

---
+++ app/src/main/java/com/example/myfirstapp/RegisterActivity.java
@@ -0,0 +1,28 @@
+package com.example.myfirstapp;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        findViewById(R.id.btn_register).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                builder.setTitle("Bartender");
+                builder.setMessage("Register Successful!");
+                builder.setNeutralButton("OK",null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+    }
+
+}
