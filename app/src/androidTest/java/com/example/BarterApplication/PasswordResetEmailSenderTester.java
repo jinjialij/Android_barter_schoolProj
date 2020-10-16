543f1d8d57fd6c4ed85755baaa00bbf398504932
cmattatall <36604988+cmattatall@users.noreply.github.com>
2020-10-16 18:51:48 -0300

    write failing tests for password reset email status message

--- app/src/androidTest/java/com/example/BarterApplication/PasswordResetEmailSenderTester.java
+++ app/src/androidTest/java/com/example/BarterApplication/PasswordResetEmailSenderTester.java
@@ -1,5 +1,6 @@
 package com.example.BarterApplication;
 
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -15,6 +16,7 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
@@ -46,7 +48,7 @@ public class PasswordResetEmailSenderTester {
     @Test
     public void AT_1() /* Layout test */
     {
-        onView(withId(R.id.passwordResetButtonId)).check(matches(isDisplayed()));
+        onView(withId(R.id.passwordResetSendEmailButtonId)).check(matches(isDisplayed()));
         onView(withId(R.id.resetEmailTextBoxId)).check(matches(isDisplayed()));
         onView(withId(R.id.passwordResetEmailStatusMessageTextViewId)).check(matches(isDisplayed()));
     }
@@ -55,8 +57,14 @@ public class PasswordResetEmailSenderTester {
     public void AT_2() /* Test the password reset email status message */
     {
         onView(withId(R.id.resetEmailTextBoxId)).perform(typeText("cmattatall2@gmail.com"));
+        hideKeyboard();
         onView(withId(R.id.passwordResetSendEmailButtonId)).perform(click());
         onView(withId(R.id.passwordResetEmailStatusMessageTextViewId)).check(matches(withText(R.string.passwordResetEmailSent)));
+    }
+
+    private void hideKeyboard() /* on small screen sizes, the keyboard can cover the editTexts */
+    {
+        onView(isRoot()).perform(ViewActions.closeSoftKeyboard());
     }
 
 
