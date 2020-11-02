ce13798d20c6ea4915367133a1592a2e04aa7b19
jlJJ <jl548339@dal.ca>
2020-11-02 16:37:52 -0400

    test AT0801 view my requests: add test for view my request button

---
+++ app/src/androidTest/java/com/example/BarterApplication/ViewMyRequestTest.java
@@ -0,0 +1,65 @@
+package com.example.BarterApplication;
+
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.example.BarterApplication.helpers.ValidationHelper;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(AndroidJUnit4.class)
+public class ViewMyRequestTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<MainActivity>(MainActivity.class);
+    FirebaseAuth mAuth;
+
+    @Before
+    public void setup()
+    {
+        FirebaseAuth.getInstance().signOut();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        String email = "jl548339@dal.ca";
+        String pass = "123456";
+        mAuth = FirebaseAuth.getInstance();
+
+        onView(withId(R.id.editTextTextEmailAddress))
+                .perform(click())
+                .perform(typeText(email), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.editTextTextPassword))
+                .perform(click())
+                .perform(typeText(pass), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.buttonLogin))
+                .perform(click());
+
+        onView(isRoot()).perform(TestHelper.waitFor(5000));
+    }
+
+    @Test
+    public void testViewMyRequestButton_AT_08_01(){
+        onView(withId(R.id.viewMyRequestBtn)).check(matches(isDisplayed()));
+        onView(withId(R.id.viewMyRequestBtn)).check(matches(isClickable()));
+    }
+
+    @After
+    public void teardown()
+    {
+        FirebaseAuth.getInstance().signOut();
+    }
+}
