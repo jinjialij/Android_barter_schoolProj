73d7414c2955187908e87d2aafb292a270e5d214
Adam Mattatall <adam.mattatall@garvin-allen.com>
2020-11-02 15:41:51 -0400

    location permissions failing test

---
+++ app/src/androidTest/java/com/example/BarterApplication/Location.java
@@ -0,0 +1,51 @@
+package com.example.BarterApplication;
+
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class Location {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    FirebaseAuth mAuth;
+    public ViewAction waitFor(final long delay) {
+        return new ViewAction() {
+            @Override public Matcher<View> getConstraints() {
+                return ViewMatchers.isRoot();
+            }
+
+            @Override public String getDescription() {
+                return "wait for " + delay + "milliseconds";
+            }
+
+            @Override public void perform(UiController uiController, View view) {
+                uiController.loopMainThreadForAtLeast(delay);
+            }
+        };
+    }
+
+    @Test
+    public void AT_4_01(){
+        onView(withText("location")).check(matches(isDisplayed()));
+    }
+
+}
