697c23be229c50bb68f3417728f11808274a34b9
Yucheng <yc270707@dal.ca>
2020-11-03 10:52:45 +0800

    AT601: add test for view add button

---
+++ app/src/androidTest/java/com/example/BarterApplication/ViewAddButtonTest.java
@@ -0,0 +1,88 @@
+package com.example.BarterApplication;
+
+import android.view.View;
+
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.example.BarterApplication.helpers.ValidationHelper;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.hamcrest.Matcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+
+public class ViewAddButtonTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<MainActivity>(MainActivity.class);
+    FirebaseAuth mAuth;
+
+    public ViewAction waitFor(final long delay) {
+        return new ViewAction() {
+            @Override public Matcher<View> getConstraints() {
+                return ViewMatchers.isRoot();
+            }
+
+            @Override public String getDescription() {
+                return "wait for " + delay + "milliseconds";
+            }
+
+            @Override public void perform(UiController uiController, View view) {
+                uiController.loopMainThreadForAtLeast(delay);
+            }
+        };
+    }
+
+    @Before
+    public void setup()
+    {
+        FirebaseAuth.getInstance().signOut();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        String email = "yc270707@dal.ca";
+        String pass = "123456";
+        mAuth = FirebaseAuth.getInstance();
+
+        onView(withId(R.id.editTextTextEmailAddress))
+                .perform(click())
+                .perform(typeText(email), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.editTextTextPassword))
+                .perform(click())
+                .perform(typeText(pass), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.buttonLogin))
+                .perform(click());
+
+        onView(isRoot()).perform(waitFor(5000));
+    }
+
+    @Test
+    public void testViewMyRequestButton_AT_06_01(){
+        onView(withId(R.id.viewMyAddBtn)).check(matches(isDisplayed()));
+        onView(withId(R.id.viewMyAddBtn)).check(matches(isClickable()));
+    }
+
+    @After
+    public void teardown()
+    {
+        FirebaseAuth.getInstance().signOut();
+    }
+
+}
