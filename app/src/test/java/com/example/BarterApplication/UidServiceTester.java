91f3a38617268d6341dd9956f19dafd89ee838b1
jlJJ <jl548339@dal.ca>
2020-11-06 19:29:32 -0400

    test: add test to uidService to get objects by uid

--- app/src/test/java/com/example/BarterApplication/UidServiceTester.java
+++ app/src/test/java/com/example/BarterApplication/UidServiceTester.java
@@ -6,7 +6,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 public class UidServiceTester {
 
@@ -25,5 +29,27 @@ public class UidServiceTester {
         String first = UidService.newUID();
         String second = UidService.newUID();
         assertFalse(first.equals(second));
+    }
+
+    @Test
+    public void findItemByIdTest(){
+        ArrayList<Item> items = new ArrayList<Item>();
+        Item item = new Item("item1", "owner1");
+        items.add(item);
+        Item resultItem = UidService.findItemByItemUid(item.getUid(), items);
+        assertNotNull(resultItem);
+        assertEquals(item.getUid(), resultItem.getUid());
+    }
+
+    @Test
+    public void findItemOwnerByItemOwnerIdTest(){
+        String e = "validEmail@test.com";
+        String uid = "1234567";
+        UserAccount userAccount = new UserAccount(e, uid);
+        ArrayList<UserAccount> userAccounts = new ArrayList<UserAccount>();
+        Item item = new Item("item1", "1234567");
+        userAccounts.add(userAccount);
+        User owner = UidService.findItemOwnerByOwnerId(item.getOwnerId(), userAccounts);
+        assertEquals(item.getOwnerId(), owner.getUid());
     }
 }
