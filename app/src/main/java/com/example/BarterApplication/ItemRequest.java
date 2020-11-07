aca1ebd8dd8c3f41c8210e77ae626bed892e5238
jlJJ <jl548339@dal.ca>
2020-11-06 20:12:57 -0400

    feature: add itemRequest constructor to pass test

--- app/src/main/java/com/example/BarterApplication/ItemRequest.java
+++ app/src/main/java/com/example/BarterApplication/ItemRequest.java
@@ -5,23 +5,38 @@ import com.example.BarterApplication.helpers.UidService;
 import java.util.ArrayList;
 
 public class ItemRequest {
-        private String requesterId;
-        private ArrayList<String> ItemIdsOffered;
-        private String itemId;
+    private String requesterId;
+    private ArrayList<String> ItemIdsOffered;
+    private String requestItemId;
 
-        public ItemRequest(User u, Item wanted, ArrayList<Item> offered ) {
-            if(offered.size() != 0){
-                this.requesterId = u.getUid();
-                this.itemId = wanted.getUid();
-                this.ItemIdsOffered = new ArrayList<String>();
-                int i = 0;
-                for(i = 0; i < offered.size(); i++){
-                    if(!this.ItemIdsOffered.contains(offered.get(i).getUid())){
-                        this.ItemIdsOffered.add(offered.get(i).getUid());
-                    }
+    public ItemRequest(UserAccount u, Item wanted, ArrayList<Item> offered ) {
+        if(offered.size() != 0){
+            this.requesterId = u.getUid();
+            this.requestItemId = wanted.getUid();
+            this.ItemIdsOffered = new ArrayList<String>();
+            int i = 0;
+            for(i = 0; i < offered.size(); i++){
+                if(!this.ItemIdsOffered.contains(offered.get(i).getUid())){
+                    this.ItemIdsOffered.add(offered.get(i).getUid());
                 }
             }
         }
+    }
+
+    //This constructor is only used for FirebaseUser currentUser who log in the app
+    public ItemRequest(String requesterId, Item wanted, ArrayList<Item> offered ) {
+        if(offered.size() != 0){
+            this.requesterId = requesterId;
+            this.requestItemId = wanted.getUid();
+            this.ItemIdsOffered = new ArrayList<String>();
+            int i = 0;
+            for(i = 0; i < offered.size(); i++){
+                if(!this.ItemIdsOffered.contains(offered.get(i).getUid())){
+                    this.ItemIdsOffered.add(offered.get(i).getUid());
+                }
+            }
+        }
+    }
 
     public String getRequesterId() {
         return requesterId;
@@ -30,5 +45,9 @@ public class ItemRequest {
     public ArrayList<String> getItemIdsOffered(){
         /** @todo */
         return this.ItemIdsOffered;
+    }
+
+    public String getRequestedItemId() {
+        return requestItemId;
     }
 }
