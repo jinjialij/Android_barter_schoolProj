e5e989682e32ec41e58c0af95fdbc60cf3013709
jlJJ <jl548339@dal.ca>
2020-11-06 20:37:50 -0400

    feature: add itemRequest methods to pass constructor test for firebase use

--- app/src/main/java/com/example/BarterApplication/ItemRequest.java
+++ app/src/main/java/com/example/BarterApplication/ItemRequest.java
@@ -6,36 +6,31 @@ import java.util.ArrayList;
 
 public class ItemRequest {
     private String requesterId;
-    private ArrayList<String> ItemIdsOffered;
+    private ArrayList<String> itemIdsOffered;
     private String requestItemId;
+    private String uid;
 
     public ItemRequest(UserAccount u, Item wanted, ArrayList<Item> offered ) {
+        this.uid = UidService.newUID();
         if(offered.size() != 0){
             this.requesterId = u.getUid();
             this.requestItemId = wanted.getUid();
-            this.ItemIdsOffered = new ArrayList<String>();
+            this.itemIdsOffered = new ArrayList<String>();
             int i = 0;
             for(i = 0; i < offered.size(); i++){
-                if(!this.ItemIdsOffered.contains(offered.get(i).getUid())){
-                    this.ItemIdsOffered.add(offered.get(i).getUid());
+                if(!this.itemIdsOffered.contains(offered.get(i).getUid())){
+                    this.itemIdsOffered.add(offered.get(i).getUid());
                 }
             }
         }
     }
 
     //This constructor is only used for FirebaseUser currentUser who log in the app
-    public ItemRequest(String requesterId, Item wanted, ArrayList<Item> offered ) {
-        if(offered.size() != 0){
-            this.requesterId = requesterId;
-            this.requestItemId = wanted.getUid();
-            this.ItemIdsOffered = new ArrayList<String>();
-            int i = 0;
-            for(i = 0; i < offered.size(); i++){
-                if(!this.ItemIdsOffered.contains(offered.get(i).getUid())){
-                    this.ItemIdsOffered.add(offered.get(i).getUid());
-                }
-            }
-        }
+    public ItemRequest(String requesterId, String requestItemId, String uid) {
+        this.uid = uid;
+        this.requesterId = requesterId;
+        this.requestItemId = requestItemId;
+        this.itemIdsOffered = new ArrayList<>();
     }
 
     public String getRequesterId() {
@@ -44,10 +39,22 @@ public class ItemRequest {
 
     public ArrayList<String> getItemIdsOffered(){
         /** @todo */
-        return this.ItemIdsOffered;
+        return this.itemIdsOffered;
     }
 
     public String getRequestedItemId() {
         return requestItemId;
+    }
+
+    public String getUid() {
+        return this.uid;
+    }
+
+    public void addOfferedItemIds(ArrayList<String> offeredItemIds){
+        for(String id : offeredItemIds){
+            if(!this.itemIdsOffered.contains(id)){
+                this.itemIdsOffered.add(id);
+            }
+        }
     }
 }
