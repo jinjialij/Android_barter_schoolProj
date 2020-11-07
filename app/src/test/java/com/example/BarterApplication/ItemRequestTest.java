7a1b5f90ee68eb8a0eb389e9fa738a6843a0ad41
jlJJ <jl548339@dal.ca>
2020-11-06 20:09:12 -0400

    test: add test for itemRequest constructor for firebase use

--- app/src/test/java/com/example/BarterApplication/ItemRequestTest.java
+++ app/src/test/java/com/example/BarterApplication/ItemRequestTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 public class ItemRequestTest {
@@ -42,4 +43,28 @@ public class ItemRequestTest {
         }
     }
     */
+
+    @Test
+    public void test_CTOR_for_firebase_currentUser_use(){
+        //in this case, user is the requester
+        String userId = "userId";
+        String name = "my item";
+        Item itemBeingRequested = new Item(name, userId);
+        String offered_item_name1 = "item_to_offer1";
+        String offered_item_name2 = "item_to_offer2";
+        ArrayList<Item> itemsOffered = new ArrayList<Item>();
+        Item offer1 = new Item(offered_item_name1, userId);
+        Item offer2 = new Item(offered_item_name2, userId);
+        itemsOffered.add(offer1);
+        itemsOffered.add(offer2);
+        ItemRequest request = new ItemRequest(userId, itemBeingRequested, itemsOffered);
+        assertEquals(request.getRequestedItemId(), itemBeingRequested.getUid());
+        assertEquals(request.getRequesterId(), userId);
+        assertEquals(request.getItemIdsOffered().size(), itemsOffered.size());
+
+        ArrayList<String> itemIdsOffered = new ArrayList<String>();
+        itemIdsOffered.add(offer1.getUid());
+        itemIdsOffered.add(offer2.getUid());
+        assertTrue(itemIdsOffered.containsAll(request.getItemIdsOffered()));
+    }
 }
