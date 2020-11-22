8d7297d8821c906ed327eacb5beebcab50109ec3
cmattatall <36604988+cmattatall@users.noreply.github.com>
2020-11-22 15:31:04 -0400

    add very preliminary BarterActivity class (no tests written yet)

--- app/src/main/java/com/example/BarterApplication/BarterActivity.java
+++ app/src/main/java/com/example/BarterApplication/BarterActivity.java
@@ -1,10 +1,42 @@
 package com.example.BarterApplication;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BarterActivity extends AppCompatActivity {
 
-    public BarterActivity(){
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barter);
+    }
+
+    /**
+     * @brief return to the homepage activity
+     * @param v The current layout content view for the activity
+     */
+    public void goBackToHomepage(View v) {
+
+    }
+
+    /**
+     * @brief send a barter request to the owner of the currently displayed item
+     * @param v The current layout content view for the activity
+     * @note ANALOGOUS TO SWIPE RIGHT (or click the green heart button) in tinder - carl
+     */
+    public void sendRequest(View v) {
+
+    }
+
+
+    /**
+     * @brief go to the next item
+     * @param v The current layout content view for the activity
+     * @note ANALOGOUS TO SWIPE LEFT (or click the red X button) in tinder - carl
+     */
+    public void nextItem(View v){
 
     }
 }
